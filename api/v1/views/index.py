b25d0eb690b38842ce1d2877528d19250bb0be27
faykris <faykris28@gmail.com>
2021-09-18 22:43:17 -0500

    update index.py - change return jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from models import storage
 @app_views.route("/status", strict_slashes=False)
 def status():
     """"Function that return a status on JSON dictionary"""
-    return jsonify(status="OK")
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", strict_slashes=False)
